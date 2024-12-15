aada0183526753f5157648ce6fc5a72dbc401ac7
sam <shendyapin.s@gmail.com>
2024-12-15 19:02:16 +0100

    added release

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -17,7 +17,7 @@ import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
   standalone: true
 })
 export class PortfolioComponent implements OnInit {
-  filters = ['All', 'Nature', 'Sports', 'Documentary', 'Real Estate'];
+  filters = ['All', 'Nature', 'Events', 'Documentary', 'Real Estate'];
   activeFilter = 'All';
   breakpoint: number = (window.innerWidth <= 500) ? 1 : 4;
 
@@ -56,10 +56,48 @@ export class PortfolioComponent implements OnInit {
         }
       ],
     },
+    {
+      type: 'Events',
+      title: 'Events Videos',
+      description: 'Attending and showing the stories of the outdoor and indoor events.',
+      alternateLayout: false,
+      videos: [
+        {id: 1, src: '/events/NF_teaser.mp4', colspan: 1, rowspan: 2},
+        {id: 2, src: '/events/chamonix_vertical.mp4', colspan: 1, rowspan: 2},
+        {
+          id: 6, src: '', extraInfo: `
+          <p>Attending the outdoor events from different brands and capturing them without disturbing the event flows.</p>
+          <p>Great for creating the recap videos, documentary style videos, taking interviews from athletes who
+          attended the events or using it for the future promotional materials.</p>
+        `, colspan: 2, rowspan: 1
+        },
+        {id: 4, src: '/events/chamonix_horizontal.mp4', colspan: 2, rowspan: 1}
+      ],
+    },
+    {
+      type: 'Documentary',
+      title: 'Documentary Videos',
+      description: 'Documenting One or Multi-day adventure, Athlete Stories and Trainings.',
+      alternateLayout: false,
+      videos: [
+        {id: 1, src: '/adventures/via_ferrata_nastya.mp4', colspan: 1, rowspan: 2},
+        {
+          id: 6, src: '', extraInfo: `
+          <p>No matter what it is - training in the gym, via ferrata in Spain, training on the glacier or mutipitch climb</p>
+          <p>I can follow the athlete to their stories, or participate in clients trainings and adventures to share the perspective of how it was.</p>
+          <p>This is a great opportunity for creating the recap videos, documentary style videos, taking interviews from athletes who
+          attended the events or using it for the future promotional materials.</p>
+        `, colspan: 2, rowspan: 1
+        },
+        {id: 2, src: '/adventures/strongman_ulfr.mp4', colspan: 1, rowspan: 2},
+        {id: 4, src: '/adventures/glacier_training_5min.mp4', colspan: 2, rowspan: 1}
+      ],
+    },
+
     // {
     //   type: 'Sports',
     //   title: 'Sports Videos',
-    //   description: 'Capturing the thrill of action-packed sports and athletes.',
+    //   description: 'Capturing the thrill of action-packed sports and athletes. / Telling the stories of athletes with impactful visuals.',
     //   alternateLayout: true,
     //   videos: [
     //     {id: 1, src: '/background-video.mp4', colspan: 1, rowspan: 1},
@@ -77,30 +115,7 @@ export class PortfolioComponent implements OnInit {
     //     {id: 5, src: '/why_video.mp4', colspan: 2, rowspan: 2},
     //   ],
     // },
-    // {
-    //   type: 'Documentary',
-    //   title: 'Documentary Videos',
-    //   description: 'Telling real stories with impactful visuals and soundscapes.',
-    //   alternateLayout: false,
-    //   videos: [
-    //     {id: 1, src: '/why_video.mp4', colspan: 2, rowspan: 1},
-    //     {id: 2, src: '/background-video.mp4', colspan: 1, rowspan: 2},
-    //     {id: 3, src: '/why_video.mp4', colspan: 1, rowspan: 1},
-    //     {id: 4, src: '/background-video.mp4', colspan: 2, rowspan: 2},
-    //     {id: 5, src: '/background-video.mp4', colspan: 1, rowspan: 1},
-    //     {
-    //       id: 6, src: '', extraInfo: `
-    //     <p>This section highlights the serene beauty of nature, showcasing lush landscapes and peaceful vistas.</p>
-    //     This section highlights the serene beauty of nature, showcasing lush landscapes and peaceful vistas.
-    //     This section highlights the serene beauty of nature, showcasing lush landscapes and peaceful vistas.
-    //     This section highlights the serene beauty of nature, showcasing lush landscapes and peaceful vistas.
-    //     This section highlights the serene beauty of nature, showcasing lush landscapes and peaceful vistas.
-    //     This section highlights the serene beauty of nature, showcasing lush landscapes and peaceful vistas.
-    //     This section highlights the serene beauty of nature, showcasing lush landscapes and peaceful vistas.
-    //   `, colspan: 2, rowspan: 1
-    //     },
-    //   ],
-    // },
+
     // {
     //   type: 'Real Estate',
     //   title: 'Real Estate Videos',
